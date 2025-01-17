87e072429e6374fdbe70fc67ef682eb53be18296
anhtoan-VTI <bilong2468@gmail.com>
2025-01-17 10:28:26 +0700

    tpoads
    toan

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import Blanh from './Blanh/Blanh'
 import './App.css';
+import FromElemoen from './Toan/FromElemoen';
+import Tables from './Toan/Tables';
 
 function App() {
   return (
     <div>
-      <Blanh/>
+      <Tables/>
+      {/* <FromElemoen/> */}
+      {/* <Blanh/> */}
     </div>
   );
 }
