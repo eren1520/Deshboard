f87681e2b3f824c517189e9608316d63157b1527
yerger1520 <88238730+eren1520@users.noreply.github.com>
2025-01-17 11:19:55 +0700

    Revert "helo a huy"

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
 import './App.css';
-
-import VuLong from './VuLong/Calendar'
-
-function App() {
-  return (
-    <div className="flex items-center justify-center w-full min-h-screen p-4 bg-[#181d32]">
-      <VuLong/>
-
 import UserCard from './Dung/UserCard';
 
 function App() {
   return (
     <div>
  <UserCard></UserCard>
-
     </div>
   );
 }
