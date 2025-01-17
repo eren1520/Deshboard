08eb1f67e8a9fa03f328ba6f089cd7ee1bae3d20
Ncs Long <vulongdragon2005@gmail.com>
2025-01-17 11:00:30 +0700

    helo a huy
    bai vu long

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import Blanh from './Blanh/Blanh'
 import './App.css';
+import VuLong from './VuLong/Calendar'
 
 function App() {
   return (
-    <div>
-      <Blanh/>
+    <div className="flex items-center justify-center w-full min-h-screen p-4 bg-[#181d32]">
+      <VuLong/>
     </div>
   );
 }
